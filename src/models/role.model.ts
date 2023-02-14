ed79d38f15030307d82ab7268cd8cd6fd9458dc1
nhoc20170861 <xinloimoinguoi1999@gmail.com>
2023-02-14 23:33:43 +0700

    add method to init Role collection

--- src/models/role.model.ts
+++ src/models/role.model.ts
@@ -15,4 +15,33 @@ const Role = model<IRole>('Role', roleSchema);
 // Role.create([{ name: 'user' }, { name: 'admin' }, { name: 'moderator' }])
 //     .then(() => console.log('Add role success!'))
 //     .catch((err) => console.log(`Add fail: ${err}`));
+Role.findOne({ name: 'user' })
+    .then((role) => {
+        if (!role) {
+            Role.create({ name: 'user' });
+        }
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+Role.findOne({ name: 'admin' })
+    .then((role) => {
+        if (!role) {
+            Role.create({ name: 'user' });
+        }
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+
+Role.findOne({ name: 'moderator' })
+    .then((role) => {
+        if (!role) {
+            Role.create({ name: 'user' });
+        }
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+
 export default Role;
